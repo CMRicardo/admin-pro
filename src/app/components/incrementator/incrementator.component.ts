33d9fb65c43e7a314411b09bcd0e739e5d4bde6d
CMRicardo <ricardocorrales84@gmail.com>
2023-12-11 11:07:38 -0600

    feat: :sparkles: Validations to incrementator component
    Feedback when entering out of range numbers

--- src/app/components/incrementator/incrementator.component.ts
+++ src/app/components/incrementator/incrementator.component.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementator',
   templateUrl: './incrementator.component.html',
   styles: ``
 })
-export class IncrementatorComponent {
-  public progress: number = 50
-
-  get percentage() {
-    return `${this.progress}%`
+export class IncrementatorComponent implements OnInit {
+  
+  @Input('value') public progress: number = 50
+  @Input() public btnClass: string = 'btn-primary'
+  
+  @Output() onChangeValue: EventEmitter<number> = new EventEmitter()
+  
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`
   }
 
   changeValue(value: number) {
     if (this.progress === 100 && value >= 0) {
+      this.onChangeValue.emit(100)
       return this.progress = 100
     }
     if (this.progress === 0 && value < 0) {
+      this.onChangeValue.emit(0)
       return this.progress = 0
     }
+    this.onChangeValue.emit(this.progress += value)
     return this.progress += value
+  }
+
+  onChange(newValue: number) {
+    if (newValue >= 100) {
+      this.progress = 100
+    } else if(newValue <= 0) {
+      this.progress = 0
+    } else {
+      this.progress = newValue
+    }
+    console.log({value: this.progress});
+    
+    this.onChangeValue.emit(newValue)
   }
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -6,19 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: [ './progress.component.css' ]
 })
 export class ProgressComponent {
-  public progress: number = 50
+  public progress1: number = 20
+  public progress2: number = 35
 
-  get percentage() {
-    return `${this.progress}%`
+  get percentage1() {
+    return `${this.progress1}%`
   }
 
-  changeValue(value: number) {
-    if (this.progress === 100 && value >= 0) {
-      return this.progress = 100
-    }
-    if (this.progress === 0 && value < 0) {
-      return this.progress = 0
-    }
-    return this.progress += value
+  get percentage2() {
+    return `${this.progress2}%`
   }
 }
