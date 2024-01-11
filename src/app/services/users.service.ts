c95355d171f3366901fce8083a968de601a73a44
CMRicardo <ricardocorrales84@gmail.com>
2024-01-11 13:57:54 -0600

    feat: :card_file_box: Save token to localStorage

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
 import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
 
 const baseUrl = environment.baseUrl
 
@@ -11,11 +12,21 @@ const baseUrl = environment.baseUrl
 })
 export class UsersService {
   private http = inject(HttpClient)
-  
+
   public createUser = (formData: RegisterForm) => {
     return this.http.post(`${baseUrl}/users`, formData)
+      .pipe(
+        tap(
+          (res: any) => localStorage.setItem('token', res.token)
+        )
+      )
   }
   public loginUser = (formData: LoginForm) => {
     return this.http.post(`${baseUrl}/auth`, formData)
+      .pipe(
+        tap(
+          (res: any) => localStorage.setItem('token', res.token)
+        )
+      )
   }
 }
