05ac9cbcfe71e01debf61b596916b8b385205edf
CMRicardo <ricardocorrales84@gmail.com>
2024-01-10 22:10:10 -0600

    feat: :safety_vest: Add validations to register form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     LoginComponent,
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent {
+  private formBuilder = inject(FormBuilder)
+  private passwordRegex = /^(?=.*\d)(?=.*[A-Z]).{6,}$/
 
+  private equalPasswords = (passwordName: string, confirmationName: string) => {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.get(passwordName)
+      const confirmationControl = formGroup.get(confirmationName)
+      if (Boolean(passwordControl!.value === confirmationControl!.value)) {
+        confirmationControl?.setErrors(null)
+      } else {
+        confirmationControl?.setErrors({ notEqualPass: true })
+      }
+    }
+  }
+
+  public registerForm = this.formBuilder.group({
+    name: ['Ricardo', [Validators.required]],
+    email: ['test100@email.com', [Validators.required, Validators.email]],
+    password: [, [Validators.required, Validators.pattern(this.passwordRegex)]],
+    passwordConfirm: [, [Validators.required]],
+    terms: [, [Validators.required, Validators.requiredTrue]],
+  }, {
+    validators: this.equalPasswords('password', 'passwordConfirm')
+  })
+
+  public createUser = () => {
+    console.log(this.registerForm) 
+    if (this.registerForm.valid) {
+      console.log('All right');
+    } else {
+      console.log('Not so right');
+      
+    }
+  }
+
+  public notValidField = (field: string): boolean => {
+    return Boolean(
+      this.registerForm.get(field)?.invalid &&
+      this.registerForm.get(field)?.touched
+    )
+  }
+
+  public areNotEqualPasswords = () => {
+    const password = this.registerForm.get('password')!.value
+    const confirmation = this.registerForm.get('passwordConfirm')!.value
+
+    return !(password === confirmation)
+  }
 }
