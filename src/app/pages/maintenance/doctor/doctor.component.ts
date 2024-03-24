b9f6fe3cb18a73402d0673c4e28f3cd0faa209e1
CMRicardo <ricardocorrales84@gmail.com>
2024-03-24 10:43:38 -0600

    feat: :construction: Advance in create doctor form

--- src/app/pages/maintenance/doctor/doctor.component.ts
+++ src/app/pages/maintenance/doctor/doctor.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Hospital } from '@src/app/models/hospital.model'
+import { HospitalsService } from '@src/app/services/hospitals.service'
 
 @Component({
   selector: 'app-doctor',
   templateUrl: './doctor.component.html',
   styles: ``
 })
-export class DoctorComponent {
+export class DoctorComponent implements OnInit {
+  private formBuilder = inject(FormBuilder)
+  private hospitalsService = inject(HospitalsService)
 
+  public doctorForm!: FormGroup
+  public hospitals: Hospital[] = []
+
+  ngOnInit(): void {
+    this.doctorForm = this.formBuilder.group({
+      name: ['Alejandro', Validators.required],
+      hospital: ['', Validators.required]
+    })
+
+    this.fetchHospitals()
+  }
+
+  public fetchHospitals() {
+    this.hospitalsService.fetchHospitals().subscribe({
+      next: hospitals => {
+        this.hospitals = hospitals
+      }
+    })
+  }
+
+  public saveDoctor() {
+    console.log(this.doctorForm.value)
+  }
 }
