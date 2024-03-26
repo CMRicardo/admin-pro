eb4d2a4d80c974db98b59530c6b7e1174daa6bcf
CMRicardo <ricardocorrales84@gmail.com>
2024-03-26 11:09:45 -0600

    refactor: Add FormsModule to SharedModule
    And format shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { RouterModule } from '@angular/router';
-
-
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component'
+import { HeaderComponent } from './header/header.component'
+import { SidebarComponent } from './sidebar/sidebar.component'
 
 @NgModule({
-  declarations: [
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-  ]
+  declarations: [BreadcrumbsComponent, SidebarComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, FormsModule],
+  exports: [BreadcrumbsComponent, SidebarComponent, HeaderComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
