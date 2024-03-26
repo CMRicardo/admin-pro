38ed8cfbe64181f4e081b9d94b12f6529dc3f87a
CMRicardo <ricardocorrales84@gmail.com>
2024-03-26 13:27:25 -0600

    style: :art: Format sidebar.service.ts

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,32 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SidebarService {
-
-  menu: any[] = [
+  public menu = [
     {
       title: 'Dashboard',
       icon: 'mdi mdi-gauge',
       submenu: [
-        { title: 'Main', url:'/' },
-        { title: 'Charts', url:'/dashboard/chart-1' },
-        { title: 'ProgressBar', url:'/dashboard/progress' },
-        { title: 'Promises', url:'/dashboard/promises' },
-        { title: 'Rxjs', url:'/dashboard/rxjs' },
+        { title: 'Main', url: '/' },
+        { title: 'Charts', url: '/dashboard/chart-1' },
+        { title: 'ProgressBar', url: '/dashboard/progress' },
+        { title: 'Promises', url: '/dashboard/promises' },
+        { title: 'Rxjs', url: '/dashboard/rxjs' }
       ]
     },
     {
       title: 'Maintenance',
       icon: 'mdi mdi-folder-lock-open',
       submenu: [
-        { title: 'Users', url:'users' },
-        { title: 'Hospitals', url:'hospitals' },
-        { title: 'Doctors', url:'doctors' },
+        { title: 'Users', url: 'users' },
+        { title: 'Hospitals', url: 'hospitals' },
+        { title: 'Doctors', url: 'doctors' }
       ]
     }
   ]
-
-  constructor() { }
 }
